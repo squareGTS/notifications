963475b51f7d350b750a537a835550b10c95d383
Maxim <68609996+squareGTS@users.noreply.github.com>
2023-02-16 17:59:42 +0300

    push notification with context

--- notifications/notifications/ViewController.swift
+++ notifications/notifications/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, UITextFieldDelegate {
     
     let notifications = Notifications()
     
