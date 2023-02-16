bca896152b15b4cf461030af395e68330bd6040f
Maxim <68609996+squareGTS@users.noreply.github.com>
2023-02-16 10:59:28 +0300

    add image to notification

---
+++ notifications/notifications/Notifications.swift
@@ -0,0 +1,119 @@
+//
+//  Notifications.swift
+//  notifications
+//
+//  Created by Maxim Bekmetov on 16.02.2023.
+//
+
+import UIKit
+import UserNotifications
+
+class Notifications: NSObject, UNUserNotificationCenterDelegate {
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    
+    func requestAutorization() {
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            print("Permission granted: \(granted)")
+            
+            guard granted else { return }
+            self.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+        notificationCenter.getNotificationSettings { (settings) in
+            print("Notification settings: \(settings)")
+        }
+    }
+    
+    func scheduleNotification(notifaicationType: String) {
+        
+        let content = UNMutableNotificationContent()
+        let userAction = "User Action"
+        
+        content.title = notifaicationType
+        content.body = "This is example how to create " + notifaicationType
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        content.categoryIdentifier = userAction
+        
+        
+        // check if image exist
+        guard let path = Bundle.main.path(forResource: "favicon", ofType: "png") else { return }
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let attachment = try UNNotificationAttachment(
+                identifier: "favicon",
+                url: url,
+                options: nil)
+            
+            content.attachments = [attachment]
+        } catch {
+            print("The attachment cold not be loaded")
+        }
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let identifire = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifire,
+                                            content: content,
+                                            trigger: trigger)
+        
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+        
+        // add actions to notification (max 4)
+        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
+        let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
+        let category = UNNotificationCategory(
+            identifier: userAction,
+            actions: [snoozeAction, deleteAction],
+            intentIdentifiers: [],
+            options: [])
+        
+        notificationCenter.setNotificationCategories([category])
+    }
+    
+    // local notification
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            
+            completionHandler([.banner, .sound])
+        }
+    
+    // when u need tap on notification (redirect to application)
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void) {
+            
+            if response.notification.request.identifier == "Local Notification" {
+                print("Handling notification with the Local Notification Identifire")
+            }
+            
+            // type of actions for notification
+            switch response.actionIdentifier {
+            case UNNotificationDismissActionIdentifier:
+                print("Dismiss Action")
+            case UNNotificationDefaultActionIdentifier:
+                print("Default")
+            case "Snooze":
+                print("Snooze")
+                scheduleNotification(notifaicationType: "Reminder")
+            case "Delete":
+                print("Delete")
+            default:
+                print("Unknown action")
+            }
+            
+            completionHandler()
+        }
+}
